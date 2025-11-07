5ad58adc6b5d4768fc090249de7858fce9a6a47f
MawarKusumaPutri <mawarkusumaputri4@gmail.com>
2025-11-07 19:55:07 +0700

    menambakan data siswa di TU

--- resources/views/tu/jadwal/create.blade.php
+++ resources/views/tu/jadwal/create.blade.php
@@ -5,72 +5,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <!-- Sidebar -->
-        <div class="col-md-3 col-lg-2 d-md-block sidebar">
-            <div class="position-sticky pt-3">
-                <div class="text-center mb-4">
-                    <div class="profile-circle">
-                        <i class="fas fa-user-tie"></i>
-                    </div>
-                    <h6 class="text-white mt-2 mb-1">Tenaga Usaha</h6>
-                    <small class="text-white-50">Administrasi</small>
-                </div>
-                
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.dashboard') }}">
-                            <i class="fas fa-tachometer-alt"></i> Dashboard
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.guru.index') }}">
-                            <i class="fas fa-chalkboard-teacher"></i> Data Guru
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.siswa.index') }}">
-                            <i class="fas fa-users"></i> Data Siswa
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.presensi.index') }}">
-                            <i class="fas fa-calendar-check"></i> Presensi
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="{{ route('tu.jadwal.index') }}">
-                            <i class="fas fa-calendar"></i> Jadwal
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.kalender.index') }}">
-                            <i class="fas fa-calendar-alt"></i> Kalender
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.arsip.index') }}">
-                            <i class="fas fa-archive"></i> Arsip
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.surat.index') }}">
-                            <i class="fas fa-envelope"></i> Surat
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('tu.pengumuman.index') }}">
-                            <i class="fas fa-bullhorn"></i> Pengumuman
-                        </a>
-                    </li>
-                </ul>
-                
-                <div class="mt-auto">
-                    <a class="nav-link" href="{{ route('logout') }}">
-                        <i class="fas fa-sign-out-alt me-2"></i> Logout
-                    </a>
-                </div>
-            </div>
-        </div>
+        @include('partials.tu-sidebar')
 
         <!-- Main content -->
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
